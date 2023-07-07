339b54c234a5503f11b1328f6cc00962afd5dda4
jugne <ugne.stolz@bsse.ethz.ch>
2023-07-07 10:51:35 +0200

    Cleaned a bit

--- src/sr/speciation/SRangesBirthDeathModel.java
+++ src/sr/speciation/SRangesBirthDeathModel.java
@@ -22,11 +22,6 @@ import sr.evolution.sranges.StratigraphicRange;
         "Bayesian total-evidence dating under the fossilized birth-death model with stratigraphic ranges.")
 public class SRangesBirthDeathModel extends SABirthDeathModel {
 
-//    final public Input<SRTree> treeInput = new Input<>("srTree",
-//            "beast.tree on which this operation is performed",
-//            Input.Validate.REQUIRED);
-
-
     @Override
     public double q(double t, double c1, double c2) {
         double v = Math.exp(-c1 * t);
@@ -44,11 +39,6 @@ public class SRangesBirthDeathModel extends SABirthDeathModel {
 
     private double log_q_tilde(double t, double c1, double c2) {
         return 0.5*(-t*(lambda + mu + psi) + log_q(t,c1,c2));
-    }
-
-    private double log_lambda_times_int_limits_p(double tOld, double tYoung, double c1, double c2) {
-        return Math.log((lambda+mu+psi-c1)*(tOld - tYoung) + 2*Math.log(Math.exp(-c1*tYoung)*(1-c2)+(1+c2)) -
-                2*Math.log(Math.exp(-c1*tOld)*(1-c2)+(1+c2))) - Math.log(2);
     }
 
     private Node findAncestralRangeLastNode(Node node) {
@@ -86,33 +76,7 @@ public class SRangesBirthDeathModel extends SABirthDeathModel {
             return Double.NEGATIVE_INFINITY;
         }
 
-        //double x0 = tree.getRoot().getHeight() + origToRootDistance;
         double x0 = origin;
-
-//        if (taxonInput.get() != null) { //TODO rewrite this part for SA sRanges
-//
-//            if (taxonAge > origin) {
-//                return Double.NEGATIVE_INFINITY;
-//            }
-//            double logPost = 0.0;
-//
-//            if (conditionOnSamplingInput.get()) {
-//                logPost -= Math.log(oneMinusP0(x0, c1, c2));
-//            }
-//
-//            if (conditionOnRhoSamplingInput.get()) {
-//                logPost -= Math.log(oneMinusP0Hat(x0, c1, c2));
-//            }
-//
-//            if (SATaxonInput.get().getValue() == 0) {
-//                logPost += Math.log(1 - oneMinusP0(taxonAge, c1, c2));
-//            } else {
-//                logPost += Math.log(oneMinusP0(taxonAge, c1, c2));
-//            }
-//
-//            return logPost;
-//        }
-
         double x1=tree.getRoot().getHeight();
 
         if (x0 < x1 ) {
@@ -148,9 +112,6 @@ public class SRangesBirthDeathModel extends SABirthDeathModel {
             if (node.isLeaf()) {
                 if  (!node.isDirectAncestor())  {
                     Node fossilParent = node.getParent();
-//                    Node sibling = fossilParent.getChild(0).getNr()==i ? fossilParent.getChild(1) : fossilParent.getChild(0);
-//                    if (tree.directAncestorInSRange(i))
-//                        return Double.NEGATIVE_INFINITY;
                     if (node.getHeight() > 0.000000000005 || rho == 0.) {
 
                         if (((SRTree)tree).belongToSameSRange(i, fossilParent.getNr())) {
@@ -193,10 +154,8 @@ public class SRangesBirthDeathModel extends SABirthDeathModel {
                 double tOld = ancestralLast.getHeight();
                 double tYoung = first.getHeight();
                 logP += Math.log(1-q(tYoung, c1, c2)/q_tilde(tYoung, c1, c2)*q_tilde(tOld, c1, c2)/q(tOld, c1, c2));
-//                logP += log_lambda_times_int_limits_p(tOld, tYoung, c1, c2);
             }
         }
-
         return logP;
     }
 
